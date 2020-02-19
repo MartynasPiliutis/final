601d035a76373c3eea26be4f98bb256556172700
MartynasPiliutis <46004570+MartynasPiliutis@users.noreply.github.com>
2020-02-19 19:23:08 +0200

    EventRepository.cs eventList and some methods
    EventRepositoryTest.cs

---
+++ PCMLibrary/Event.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace PCMLibrary
+{
+    public class Event
+    {
+        public long EventId { get; private set; }
+        public int EmployeeId { get; private set; }
+        public int GateId { get; private set; }
+        public DateTime PassTime { get; private set; }
+        public bool Pass { get; private set; }
+
+        public Event(long eventId, int employeeId, int gateId, DateTime passtime, bool pass)
+        {
+            EventId = eventId;
+            EmployeeId = employeeId;
+            GateId = gateId;
+            PassTime = passtime;
+            Pass = pass;
+        }
+    }
+}
---
+++ PCMTest/EventRepositoryTest.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using System.Collections.Generic;
+using PCMLibrary;
+
+namespace PCMTest
+{
+    [TestClass]
+    public class EventRepositoryTest
+    {
+        [TestMethod]
+        public void PatikrinameArGrazinaVisaSarasa()
+        {
+            EventRepository eventRepository = new EventRepository();
+            int EventListCount = eventRepository.GetAllEventList().Count;
+            Assert.AreEqual(EventListCount, 4000);
+        }
+
+        [TestMethod]
+        public void PatikrinameArGrazinaVisaSarasaPagalId()
+        {
+            EventRepository eventRepository = new EventRepository();
+            int EventListCount = eventRepository.GetAllEventListByEmployeeId(2010).Count;
+            Assert.AreEqual(EventListCount, 203);
+        }
+
+        [TestMethod]
+        public void PatikrinameArGrazinaVisaSarasaPagalIdSuTrueIvykiais()
+        {
+            EventRepository eventRepository = new EventRepository();
+            int EventListCount = eventRepository.GetAllEventListByEmployeeIdThatPassed(2020).Count;
+            Assert.AreEqual(EventListCount, 105);
+        }
+    }
+}
